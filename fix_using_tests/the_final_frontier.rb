a5ad462ef5972f27368fc474ebe607ebacebe3ad
Anshul Arora <a.99.anshul@gmail.com>
2020-09-11 19:28:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,11 +1,13 @@
 require 'pry'
 
-star_date = (rand(100000) + 400000) / 10.0
+def generate_star_date
+  star_date = (rand(100000) + 400000) / 10.0
+end
+
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
-
 
 def engage
   puts state_log(date)
